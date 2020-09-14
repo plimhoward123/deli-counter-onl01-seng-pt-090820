d077e9d74e81bcde5bfd12fa3d6b9e2a3efd2e23
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 21:31:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(arry)
   if (arry.length < 1)
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     arry.each do |customer, index|
-      return "Welcome, #{customer}. You are number #{index + 1} in line."
+      puts "Welcome, #{customer}. You are number #{index + 1} in line."
 end
