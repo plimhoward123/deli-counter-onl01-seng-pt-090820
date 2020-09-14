603241e1a2d80ba4e901bc788cff5df62538dce0
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 21:29:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,9 @@ katz_deli[] = Array.new
 def line(arry)
   if (arry.length < 1)
     return "The line is currently empty."
-  end
   else
     arry.each do |customer, index|
-      puts "Welcome, #{customer}. You are number #{index + 1} in line."
+      return "Welcome, #{customer}. You are number #{index + 1} in line."
   end
 end
 
