6174ba57d6d062494d26dbc293768b61f23fde7b
Plimon Howard V <plimon.howard@gmail.com>
2020-09-14 21:32:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,14 @@
 katz_deli = []
 
 def line(arry)
+  
   if (arry.length < 1)
     puts "The line is currently empty."
   else
     arry.each do |customer, index|
       puts "Welcome, #{customer}. You are number #{index + 1} in line."
+  end
+  
 end
 
 
